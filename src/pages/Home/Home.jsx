6b349c1565f101da17b860ddb70b2e49c3b43e8e
NicoElMaster <nmorenoj@itc.edu.co>
2025-05-05 12:13:07 -0500

    Se añade barra superior y el header principal

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,39 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+ 
+    
+      <div style={{ fontFamily: "Arial, sans-serif" }}>
+        {/* Barra superior */}
+        <table width="100%" cellPadding="10" style={{ backgroundColor: "#D29BFD" }}>
+          <tbody>
+            <tr>
+              <td align="left">
+                Personas | Negocios | Corporativos | Negocios especializados | Tu360 | Blog
+              </td>
+              <td align="right">
+                Transparencia | Consumidor
+              </td>
+            </tr>
+          </tbody>
+        </table>
+  
+        {/* Header principal */}
+        <table width="100%" cellPadding="15">
+          <tbody>
+            <tr>
+              <td><strong>Galaxy Pay</strong></td>
+              <td align="center">
+                Inicio | Necesidades | Productos y Servicios | Educación Financiera | <button><strong>Trámites digitales</strong></button>
+              </td>
+              <td align="right">
+                Sucursal Virtual Personas | <button><strong>Entrar</strong></button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+  
+        
+      </div>
+    );
+  };
