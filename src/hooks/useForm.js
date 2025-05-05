3f5fac12cbcb49ec1bd1ec5155629e53ab8b3309
Juan Camilo Ayala <117867605+ThJuanK@users.noreply.github.com>
2025-05-05 12:34:45 -0500

    Inicio de la estructura del login personas

---
+++ src/hooks/useForm.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+// Hook creado inicialmente para el formulario de login, pero se puede reutilizar en cualquier formulario
+// Author: ThJuanK
+export const useForm = ( initialState = {} ) => {
+  const [ formValues, setFormValues ] = useState( initialState );
+
+  const handleInputChange = ( { target } ) => {
+    setFormValues( {
+      ...formValues,
+      [ target.name ]: target.value
+    } );
+  };
+
+  return { formValues, handleInputChange };
+}
