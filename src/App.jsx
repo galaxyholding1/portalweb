5a2dc7afde9abfd0402c2ed896c8273bd9593962
Juan Camilo Ayala <117867605+ThJuanK@users.noreply.github.com>
2025-05-07 16:40:52 -0500

    Diseño del login portal personas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Navbar } from "./components/Navbar/Navbar";
 import "./App.css";
 import { Router } from "./routes/Router";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar/>
       <Router/>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <hr />
+      <div class="footer-links">
+        <a href="#">Politicas de privacidad</a>
+        <a href="#">Reglamento portal virtual</a>
+      </div>
+      <div className="footer-web-link">
+        <a href="#">www.galaxypay.com</a>
+      </div>
+    </footer>
+  );
+};
