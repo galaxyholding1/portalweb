c70e23d1464b0962d36812f8782b3279db0a6800
Yhonatan <ytapias2390@gmail.com>
2025-05-13 12:27:26 -0500

    DESING card security right
    filter: brightness(0) saturate(100%) invert(20%) sepia(0%) saturate(0%) hue-rotate(207deg) brightness(94%) contrast(94%);

---
+++ src/pages/HomeBusiness/SecurityBusiness.jsx
@@ -0,0 +1,19 @@
+import { BalanceCard } from "../../components/home/BalanceCard/BalanceCard";
+import { MovementCard } from "../../components/home/MovementCard/MovementCard";
+import { RequestsCard } from "../../components/home/MyRequestsCard/RequestsCard";
+import { SecurityCard } from "../../components/home/SecurityCard/SecurityCard";
+import SubNavbar from "../../components/home/SubNavbar/SubNavbar";
+import "../HomePeople/HomePeople.css";
+
+export const SecurityBusiness = () => {
+  return (
+    <div className="page-container">
+      <SubNavbar />
+      <div className="dashboard-grid">
+        <div className="grid-area-applications">
+          <SecurityCard />
+        </div>
+      </div>
+    </div>
+  );
+};
