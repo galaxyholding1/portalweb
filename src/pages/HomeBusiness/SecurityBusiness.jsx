b4c4b78b9031f18d3a300b2f53bb0532fa1d6c95
Yhonatan <ytapias2390@gmail.com>
2025-05-13 14:54:09 -0500

    add card security profile

--- src/pages/HomeBusiness/SecurityBusiness.jsx
+++ src/pages/HomeBusiness/SecurityBusiness.jsx
@@ -2,6 +2,8 @@ import { BalanceCard } from "../../components/home/BalanceCard/BalanceCard";
 import { MovementCard } from "../../components/home/MovementCard/MovementCard";
 import { RequestsCard } from "../../components/home/MyRequestsCard/RequestsCard";
 import { SecurityCard } from "../../components/home/SecurityCard/SecurityCard";
+
+import { SecurityCardLeft } from "../../components/home/SecurityCard/SecurityCardLeft";
 import SubNavbar from "../../components/home/SubNavbar/SubNavbar";
 import "../HomePeople/HomePeople.css";
 
@@ -10,6 +12,7 @@ export const SecurityBusiness = () => {
     <div className="page-container">
       <SubNavbar />
       <div className="dashboard-grid">
+        <SecurityCardLeft />
         <div className="grid-area-applications">
           <SecurityCard />
         </div>
