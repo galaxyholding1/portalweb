5dd00df0620f4d793dc2ef2c783e6027a8efd493
Juan Camilo Ayala <117867605+ThJuanK@users.noreply.github.com>
2025-05-13 15:51:11 -0500

    Corrección de estilos

--- src/pages/HomeBusiness/SecurityBusiness.jsx
+++ src/pages/HomeBusiness/SecurityBusiness.jsx
@@ -1,21 +1,16 @@
-import { BalanceCard } from "../../components/home/BalanceCard/BalanceCard";
-import { MovementCard } from "../../components/home/MovementCard/MovementCard";
-import { RequestsCard } from "../../components/home/MyRequestsCard/RequestsCard";
-import { SecurityCard } from "../../components/home/SecurityCard/SecurityCard";
 
-import { SecurityCardLeft } from "../../components/home/SecurityCard/SecurityCardLeft";
+import { ProfileCard } from "../../components/home/ProfileCard/ProfileCard";
+import { SecurityCard } from "../../components/home/SecurityCard/SecurityCard";
 import SubNavbar from "../../components/home/SubNavbar/SubNavbar";
-import "../HomePeople/HomePeople.css";
+import "../SecurityPeople/SecurityPeople.css";
 
 export const SecurityBusiness = () => {
   return (
     <div className="page-container">
       <SubNavbar />
-      <div className="dashboard-grid">
-        <SecurityCardLeft />
-        <div className="grid-area-applications">
-          <SecurityCard />
-        </div>
+      <div className="security-page-container">
+        <ProfileCard />
+        <SecurityCard />
       </div>
     </div>
   );
