053b8fb3ef62b94db158df98b817e26079a399da
Juan Camilo Ayala <117867605+ThJuanK@users.noreply.github.com>
2025-05-15 10:12:40 -0500

    agrego icono provisional para cuando no encuentra alguno

--- src/components/common/ui/Icon/Icon.tsx
+++ src/components/common/ui/Icon/Icon.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { useTheme } from '../../../../store/theme-store';
+import React from "react";
+import { useTheme } from "../../../../store/theme-store";
 
 // Importa todos los íconos SVG como módulos usando Vite
-const icons = import.meta.glob('../../../../assets/icons/*.svg', { eager: true });
+const icons = import.meta.glob("../../../../assets/icons/*.svg", {
+  eager: true,
+});
 
-type IconName = 'money' | 'moving_desk' | 'cents' | 'arrow_left_right';
+const notFoundIcon = (
+  icons[`../../../../assets/icons/not_found.svg`] as
+    | { default: string }
+    | undefined
+).default;
+
+type IconName = "money" | "moving_desk" | "cents" | "arrow_left_right";
 
 interface IconProps {
   name: IconName;
@@ -12,37 +20,55 @@ interface IconProps {
   filter?: string;
 }
 
+export const Icon: React.FC<IconProps> = ({
+  name,
+  selected = false,
+  filter,
+}) => {
+  const { theme } = useTheme();
 
-export const Icon: React.FC<IconProps> = ({ name, selected = false, filter }) => {
-  const {theme} = useTheme();
+  const iconName =
+    name + (theme === "dark" ? "_dark" : "") + (selected ? "-selected" : "");
+  const iconPath = icons[`../../../../assets/icons/${iconName}.svg`] as
+    | { default: string }
+    | undefined;
 
-  const iconName = name + (theme === 'dark' ? '_dark' : '') + (selected ? '-selected' : '');
-  const iconPath = icons[`../../../../assets/icons/${iconName}.svg`] as { default: string } | undefined;
-
-  const style = filter ? {filter} : {fill: 'black'};
+  const style = filter ? { filter } : { fill: "black" };
 
   if (!iconPath) {
-
     // Se busca el tema contrario y mismo select
-    let newIconName = name + (theme === 'light' ? '_dark' : '') + (selected ? '-selected' : '');
-    let newIconPath = icons[`../../../../assets/icons/${newIconName}.svg`] as { default: string } | undefined;
-    if( newIconPath ) return <img src={newIconPath.default} alt={name} style={style}/>;
-    
+    let newIconName =
+      name + (theme === "light" ? "_dark" : "") + (selected ? "-selected" : "");
+    let newIconPath = icons[`../../../../assets/icons/${newIconName}.svg`] as
+      | { default: string }
+      | undefined;
+    if (newIconPath)
+      return <img src={newIconPath.default} alt={name} style={style} />;
+
     // Se busca el tema contrario y diferente select
-    newIconName = name + (theme === 'light' ? '_dark' : '') + (!selected ? '-selected' : '');
-    newIconPath = icons[`../../../../assets/icons/${newIconName}.svg`] as { default: string } | undefined;
-    
-    if( newIconPath ) return <img src={newIconPath.default} alt={name} style={style}/>;
-    
+    newIconName =
+      name +
+      (theme === "light" ? "_dark" : "") +
+      (!selected ? "-selected" : "");
+    newIconPath = icons[`../../../../assets/icons/${newIconName}.svg`] as
+      | { default: string }
+      | undefined;
+
+    if (newIconPath)
+      return <img src={newIconPath.default} alt={name} style={style} />;
+
     // Se busca el mismo tema y diferente select
-    newIconName = name + (theme === 'dark' ? '_dark' : '') + (!selected ? '-selected' : '');
-    newIconPath = icons[`../../../../assets/icons/${newIconName}.svg`] as { default: string } | undefined;
+    newIconName =
+      name + (theme === "dark" ? "_dark" : "") + (!selected ? "-selected" : "");
+    newIconPath = icons[`../../../../assets/icons/${newIconName}.svg`] as
+      | { default: string }
+      | undefined;
 
-    if( newIconPath ) return <img src={newIconPath.default} alt={name} style={style}/>;
+    if (newIconPath)
+      return <img src={newIconPath.default} alt={name} style={style} />;
 
-    
-    throw new Error(`Se está usando un icono que no existe: ${name}`);
+    return <img src={notFoundIcon} alt={name} style={style} width={24} />;
   }
 
-  return <img src={iconPath.default} alt={name} style={style}/>;
+  return <img src={iconPath.default} alt={name} style={style} />;
 };
