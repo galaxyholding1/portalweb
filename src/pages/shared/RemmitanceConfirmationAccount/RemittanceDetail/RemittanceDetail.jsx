449b674991658757496fef2c316544cf2868ea1a
Juan Camilo Ayala <117867605+ThJuanK@users.noreply.github.com>
2025-05-21 16:00:33 -0500

    Ajuste de componentes para resultado de remesa

--- src/pages/shared/RemmitanceConfirmationAccount/RemittanceDetail/RemittanceDetail.jsx
+++ src/pages/shared/RemmitanceConfirmationAccount/RemittanceDetail/RemittanceDetail.jsx
@@ -3,15 +3,26 @@ import { SimpleCard } from '../../../../components/common/home/Card/SimpleCard/S
 import { CardHeader } from '../../../../components/common/home/Card/CardHeader/CardHeader'
 import { ContactHeader } from '../../../../components/common/contact/ContactHeader/ContactHeader'
 import { SaveButton } from '../../../../components/common/ui/Button/SaveButton'
+import { Button } from '../../../../components/common/ui/Button/Button'
+import { Icon } from '../../../../components/common/ui/Icon/Icon'
 
 export const RemittanceDetail = () => {
   return (
     <SimpleCard className="remittance-detail-container">
-      <ContactHeader/>
+      <div className='remittance-detail-header'>
+        <ContactHeader/>
+        <h1>Monto: 80,00 VEF</h1>
+        <Icon name='bdgeOk' width={25}/>
+      </div>
       <div>
         {/* Sisas, acá */}
       </div>
-      <SaveButton/>
+      <Button 
+        icon={<Icon name='printer'/>}
+        className="remittance-detail-button"
+      >
+        Descargar PDF
+      </Button>
     </SimpleCard>
   )
 }
