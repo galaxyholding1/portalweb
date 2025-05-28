e719324de83ac5a2f94eac1fde6861778b6d6137
NicoElMaster <nmorenoj@itc.edu.co>
2025-05-28 17:26:04 -0500

    Se realizan ajustes de diseño

--- src/pages/people/LoginPeople/LoginPeople.jsx
+++ src/pages/people/LoginPeople/LoginPeople.jsx
@@ -1,7 +1,5 @@
 import { Link, useNavigate } from "react-router";
-
 import "./LoginPeople.css";
-
 import { useForm } from "../../../hooks/useForm";
 import { PasswordInput } from "../../../components/common/ui/Input/PasswordInput";
 import { CustomInput } from "../../../components/common/ui/Input/CustomInput";
@@ -19,38 +17,40 @@ export const LoginPeople = () => {
   const handleLogin = (e) => {
     e.preventDefault();
     console.log(formValues);
-    navigate("/portal-personas/home")
+    navigate("/portal-personas/home");
   };
 
   return (
     <div className="login-screen">
       <main className="login-container">
         <form onSubmit={handleLogin}>
-          <h3>Inicio de sesión</h3>
-          <p>
-            Si no tienes un usuario asignado ingresa tu número de identidad.
+          <h3 className="login-title">Inicio de sesión</h3>
+          <p className="login-description">
+            si no tienes un usuario asignado ingresa tu número de identidad.
           </p>
-          <CustomInput
-            handleInputChange={handleInputChange}
-            value={formValues.username}
-            name="username"
-            placeholder="Usuario o DNI"
-          />
+          <div className="form-container">
+            <CustomInput
+              handleInputChange={handleInputChange}
+              value={formValues.username}
+              name="username"
+              placeholder="Usuario o DNI"
+            />
 
-          <PasswordInput
-            handleInputChange={handleInputChange}
-            value={formValues.password}
-            name="password"
-          />
+            <PasswordInput
+              handleInputChange={handleInputChange}
+              value={formValues.password}
+              name="password"
+            />
+          </div>
 
-          <button type="submit" id="btn-login-people">
-            Iniciar sesión
+          <button type="submit" className="btn-login-people">
+            iniciar sesión
           </button>
 
           <nav className="login-links">
-            <Link>¿Olvidaste tu usuario?</Link>
+            <Link>¿olvidaste tu usuario?</Link>
             <br />
-            <Link>¿Problemas para conectarte?</Link>
+            <Link>¿problemas para conectarte?</Link>
           </nav>
         </form>
 
